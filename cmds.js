a3e8bf2b065f1068e281d44b4449d61b3a0bd73b
ferruano <36645412+ferruano@users.noreply.github.com>
2018-03-03 16:09:24 +0100

    Add files via upload

---
+++ cmds.js
@@ -0,0 +1,144 @@
+const model = require('./model');
+
+const{log, biglog, errorlog, colorize} = require("./out");
+
+exports.helpCmd = rl => {
+    log("Comandos");
+    log("h|help - Muestra esta ayuda");
+    log("list - Listar los quizzes existentes");
+    log("show <id> - Muestra la pregunta y la respuesta el quiz indicado");
+    log("add - Añadir un nuevo quiz interactivamente");
+    log("delete <id> - Borrar el quiz indicado");
+    log("edit <id> - Editar el quiz indicado");
+    log("test <id> - Probar el quiz indicado");
+    log("p|play - Jugar a preguntar aleatoriamente todos los quizzes");
+    log("credits - Creditos.");
+    log("q|quit - Salir del programa");
+    rl.prompt();
+};
+exports.listCmd = rl => {
+	model.getAll().forEach((quiz,id) => {
+		log(`  [${colorize(id,'magenta')}]: ${quiz.question}`);
+	});
+	rl.prompt();
+};
+exports.showCmd = (rl,id) => {
+    if(typeof id === "undefined"){
+    	errorlog(`Falta el parámetro id`);
+    }else{
+    	try{
+    		const quiz = model.getByIndex(id);
+    		log(`[${colorize(id, 'magenta')}]: ${quiz.question} ${colorize('=>', 'magenta')}${quiz.answer}`);
+    	} catch(error){
+    		errorlog(error.message);
+    	}
+    }
+    rl.prompt();
+};
+exports.addCmd = rl => {
+
+	rl.question(colorize(' Introduzca una pregunta: ', 'red'), question => {
+		rl.question(colorize(' Introduzca una respuesta: ', 'red'), answer => {
+			model.add(question,answer);
+			log(` ${colorize('Se ha añadido', 'magenta')}: ${question} ${colorize('=>', 'magenta')} ${answer}`);
+			rl.prompt();
+		});
+	});
+};
+exports.deleteCmd = (rl,id) => {
+	if(typeof id === "undefined"){
+    	errorlog(`Falta el parámetro id`);
+    }else{
+    	try{
+    		model.deleteByIndex(id);
+    	} catch(error){
+    		errorlog(error.message);
+    	}
+    }
+    rl.prompt();
+};
+exports.editCmd = (rl,id) => {
+	if(typeof id === "undefined"){
+    	errorlog(`Falta el parámetro id`);
+    }else{
+    	try{
+    		const quiz = model.getByIndex(id);
+    		process.stdout.isTTY && setTimeout(() => {rl.write(quiz.question)},0);
+ 			rl.question(colorize(' Introduzca una pregunta: ', 'red'), question => {
+				process.stdout.isTTY && setTimeout(() => {rl.write(quiz.answer)},0);
+				rl.question(colorize(' Introduzca una respuesta: ', 'red'), answer => {
+					model.add(question,answer);
+					log(` Se ha cambiado el quiz ${colorize(id, 'magenta')} por:  ${question} ${colorize('=>', 'magenta')} ${answer}`);
+					rl.prompt();
+				});
+			});
+    		
+    	} catch(error){
+    		errorlog(error.message);
+    		rl.prompt();
+    	}
+    }
+};
+exports.testCmd = (rl,id) => {
+	if(typeof id === "undefined"){
+    	errorlog(`Falta el parámetro id`);
+    	rl.prompt();
+    }else{
+    	try{
+    		const quiz = model.getByIndex(id);
+			rl.question(`${colorize(quiz.question,'red')}  `, respuesta => {
+				if (respuesta.toLowerCase().trim()===quiz.answer.toLowerCase().trim()) {
+					biglog('CORRECTO','green');	
+				}else{
+					biglog('INCORRECTO','red');
+				}
+			});
+    		rl.prompt();
+    	} catch(error){
+    		errorlog(error.message);
+    		rl.prompt();
+    	}
+    }
+};
+exports.creditsCmd = rl => {
+	log("Autor de la práctica: ");
+    log("Fernando Ruano Crespo",'green');
+    rl.prompt();
+};
+exports.playCmd = rl => {
+	let score =0;
+	let arrayPreguntas= [];
+	let numero = model.count();
+	for (var i = 0; i < numero ;i++) {
+		arrayPreguntas[i]=model.getByIndex(i);
+	}
+	const playOne = ()=>{
+		if (arrayPreguntas.length===0) {
+			log(`	No hay nada más que preguntar.`);
+			log(`	Fin del examen. Aciertos: `);	
+			biglog(score,'magenta');
+			rl.prompt();
+		}else{
+			let numeroAzar = Math.floor(Math.random()*arrayPreguntas.length);
+			const quiz = arrayPreguntas[numeroAzar];
+			arrayPreguntas.splice(numeroAzar,1);
+			rl.question(`${colorize(quiz.question,'red')}  `, respuesta => {
+				if (respuesta.toLowerCase().trim()===quiz.answer.toLowerCase().trim()) {
+					score++;
+					log(`	CORRECTO - Lleva una puntiación de ${colorize(score, 'magenta')}`);
+					playOne();
+				}else{
+					log(`	INCORRECTO.`);
+					log(`	Fin del examen. Aciertos: `);
+					biglog(score,'magenta');
+					rl.prompt();
+				}
+			});
+
+		}
+	};
+	playOne();
+};
+exports.quitCmd = rl => {
+	rl.close();
+};
